d146be7eaf6cb966fd998802743f2157dabec587
PeterSam <panupophahaha@gmail.com>
2025-02-03 14:52:22 +0700

    s

--- TestTest/Form1.cs
+++ TestTest/Form1.cs
@@ -16,5 +16,10 @@ namespace TestTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 3;
+        }
     }
 }
