34f833b83c7fe175dfa722fda66d65c0d74311df
PeterSam666 <110148845+PeterSam666@users.noreply.github.com>
2025-02-03 13:06:53 +0700

    Update GUI

--- TestTest/Form1.Designer.cs
+++ TestTest/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_Submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_Submit
             // 
-            this.button1.Location = new System.Drawing.Point(317, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Btn_Submit.Location = new System.Drawing.Point(317, 186);
+            this.Btn_Submit.Name = "Btn_Submit";
+            this.Btn_Submit.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Submit.TabIndex = 0;
+            this.Btn_Submit.Text = "button1";
+            this.Btn_Submit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Submit);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_Submit;
     }
 }
 
